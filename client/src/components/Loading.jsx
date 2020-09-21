ce9516a199169c0b67dc9ba482a1a66a61c9d48d
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2020-09-21 17:30:59 +0800

    Add loading component (progress indicator)
    - For convenience later

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,22 @@
+import { Box, CircularProgress, makeStyles } from '@material-ui/core';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(4),
+    textAlign: 'center'
+  }
+}));
+
+/** Circular progress indicator to be displayed while data is being fetched. */
+const Loading = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <CircularProgress size={30} />
+    </Box>
+  );
+};
+
+export default Loading;
