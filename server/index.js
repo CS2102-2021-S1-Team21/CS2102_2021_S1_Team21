b062ae336de5dc4e84160749d1a9a04560249883
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2020-09-23 23:36:18 +0800

    Fix CORS on server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
 
@@ -5,10 +6,24 @@ const app = express();
 
 // Middleware
 
+app.use(
+  cors({
+    // allow CORS for any port on localhost or example-domain.herokuapp.com
+    // TODO: replace with actual heroku domain
+    origin: /https?:\/\/(localhost:[0-9]{1,5})|(example-domain.herokuapp.com)/,
+  }),
+);
+
+// Routing
 app.use('/api', routes); // prepend all routes with '/api'
 
-// TODO: error handling for other routes
+// Generic Error Handling
+app.use((err, req, res, next) => {
+  // TODO: decide structure of error response
+  res.json({ error: 'An unexpected error occurred' });
+  next(err);
+});
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!');
+  console.log('Server is listening on port 8000!');
 });
