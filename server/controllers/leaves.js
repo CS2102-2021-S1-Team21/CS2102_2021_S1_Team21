ecdef613f68a331eb4d805b5f019694f8f8e724f
Wincent <wincenttsjoi@gmail.com>
2020-09-27 01:14:32 +0800

    Check lint

--- server/controllers/leaves.js
+++ server/controllers/leaves.js
@@ -15,17 +15,16 @@ exports.apply = async (req, res) => {
 };
 
 exports.retrieve = async (req, res) => {
-    try {
-      const result = await db.query(sql.leaves.queries.retrieve);
+  try {
+    const result = await db.query(sql.leaves.queries.retrieve);
     //   res.json({ email: result.email })
-      if (result.rowCount === 0) {
-        res.json({ error: 'No such pet owner exists' });
-        return; // TODO: next()?
-      }
-      res.json(result.rows[0]);
-    } catch (err) {
-      console.error('ERROR: ', err.message);
-      res.json({ error: 'An unexpected error occurred' });
+    if (result.rowCount === 0) {
+      res.json({ error: 'No such pet owner exists' });
+      return; // TODO: next()?
     }
-  };
-  
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error('ERROR: ', err.message);
+    res.json({ error: 'An unexpected error occurred' });
+  }
+};
--- server/sql/leaves.js
+++ server/sql/leaves.js
@@ -1,8 +1,9 @@
 const leaves = {};
 
 leaves.queries = {
-  apply: 'INSERT INTO Applies_For_Leave_Period (email, startDate, endDate) VALUES ($1, $2, $3) RETURNING *',
-  retrieve: 'SELECT * FROM Applies_For_Leave_Period'
+  apply:
+    'INSERT INTO Applies_For_Leave_Period (email, startDate, endDate) VALUES ($1, $2, $3) RETURNING *',
+  retrieve: 'SELECT * FROM Applies_For_Leave_Period',
 };
 
 module.exports = leaves;
