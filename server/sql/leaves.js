e63ee41b381b151655a062ac6e7aaef2afa150b0
Wincent <wincenttsjoi@gmail.com>
2020-09-27 01:32:22 +0800

    Linting check

--- server/sql/leaves.js
+++ server/sql/leaves.js
@@ -3,7 +3,7 @@ const leaves = {};
 leaves.queries = {
   apply:
     'INSERT INTO applies_for_leave_period (email, startDate, endDate) VALUES ($1, $2, $3) RETURNING *',
-  retrieve: 'SELECT * FROM applies_for_leave_period'
+  retrieve: 'SELECT * FROM applies_for_leave_period',
 };
 
 module.exports = leaves;
