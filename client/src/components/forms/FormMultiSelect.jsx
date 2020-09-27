1607a07020c0b20f2d62b8e89491f4ed75b884a0
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2020-09-28 00:44:21 +0800

    Add generic form components
    To enable easier development when adding forms later.
    Demo (FormTest) can be accessed at http://localhost:3000/test/forms.

---
+++ client/src/components/forms/FormMultiSelect.jsx
@@ -0,0 +1,86 @@
+import {
+  Chip,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@material-ui/core';
+import { getIn } from 'formik';
+import React from 'react';
+
+/**
+ * A styled MultiSelect that displays error messages based on Formik's current state.
+ * Additional props are forwarded to the underlying FormControl component.
+ *
+ * To be used as a custom component for Formik's <Field>, e.g.
+ * ```
+ * <Field
+ *   name="services"
+ *   label="Services Provided"
+ *   component={FormMultiSelect}
+ *   options={[
+ *     { value: 'homeVisit', label "Home Visit" }
+ *   ]}
+ * />
+ * ```
+ */
+const FormMultiSelect = ({
+  field,
+  form,
+  label,
+  options,
+  SelectProps,
+  maxItems = 3, // FIXME: find a better way to tell if selected options are too long to be displayed
+  ...otherProps
+}) => {
+  const errorMessage = getIn(form.errors, field.name);
+  const shouldShowError = getIn(form.touched, field.name) && !!errorMessage;
+  const helperText = shouldShowError ? errorMessage : otherProps.helperText;
+
+  // TODO: add flow/proptypes and make `options` required
+
+  const findLabel = (value) => options.find((option) => option.value === value)?.label;
+  const renderValue = (selected) => {
+    let chips = selected.map((value) => <Chip key={value} label={findLabel(value)} />);
+    // TODO: show all options if element is focused
+    const remaining = selected.length - maxItems;
+    if (remaining > 0) {
+      chips = chips.slice(0, maxItems);
+      chips.push(
+        <Typography key="last" display="inline" variant="body2">
+          {` +${remaining} more`}
+        </Typography>,
+      );
+    }
+    return chips;
+  };
+
+  return (
+    <FormControl variant="outlined" {...otherProps} error={shouldShowError} fullWidth>
+      <InputLabel htmlFor={field.name} id={`${field.name}-label`}>
+        {label}
+      </InputLabel>
+      <Select
+        {...field}
+        disabled={form.isSubmitting}
+        {...SelectProps} // allow the above props to be overridden
+        id={field.name}
+        labelId={`${field.name}-label`}
+        label={label} // required for layout purposes
+        multiple
+        renderValue={renderValue}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </Select>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
+  );
+};
+
+export default FormMultiSelect;
