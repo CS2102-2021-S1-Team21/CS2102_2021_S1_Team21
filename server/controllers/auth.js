228c039877a75aac7b5f686a703d3bd943c12894
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2020-10-07 02:45:37 +0800

    Refactor backend auth routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,15 +1,30 @@
+const passport = require('passport');
 // const db = require('../db');
 
 // Login
-exports.create_session = (req, res) => {
-  const { username, email, name } = req.user;
-  res.status(200).json({
-    message: `Welcome, ${req.user.name}!`,
-    data: { username, email, name },
-  });
-};
-exports.create_session_failure = (req, res) => {
-  res.status(401).json({ message: 'Invalid username or password' });
+exports.create_session = async (req, res, next) => {
+  await passport.authenticate('local', (error, user) => {
+    if (error) {
+      next(error);
+    }
+
+    // authentication failed
+    if (!user) {
+      res.status(401).json({ message: 'Invalid username or password' });
+      return;
+    }
+
+    req.login(user, (loginError) => {
+      if (loginError) {
+        return next(loginError);
+      }
+      const { username, email, name } = user;
+      return res.status(200).json({
+        message: `Welcome, ${req.user.name}!`,
+        data: { username, email, name },
+      });
+    });
+  })(req, res, next);
 };
 
 // Logout
@@ -17,6 +32,14 @@ exports.delete_session = (req, res) => {
   req.logout();
   res.status(200).json({ message: 'Good bye!' });
 };
+
+// Verify session
+// exports.session_info = (req, res) => {
+//   // get user for this session
+//   // get user info from db
+//   // send to frontend
+//   console.log(req);
+// };
 
 // Signup
 exports.create_user = (req, res, next) => {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,11 @@
 const router = require('express').Router();
-const passport = require('passport');
 const { ensureAuthenticated } = require('../auth/middleware');
 
 const auth = require('../controllers/auth');
 
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    failureRedirect: '/api/login', // redirect to GET /login if login fails
-  }),
-  auth.create_session,
-);
-router.get('/login', auth.create_session_failure);
+router.post('/login', auth.create_session);
 
-router.delete('/logout', ensureAuthenticated, auth.delete_session);
+router.delete('/logout', auth.delete_session);
 
 router.post('/signup', auth.create_user);
 
