29bf440a8f7adcfe2c97226d1046d1653b9646e1
Wincent <wincenttsjoi@gmail.com>
2020-10-07 20:56:21 +0800

    Encapsulate Formik on the entire myapplication

--- server/sql/database.sql
+++ server/sql/database.sql
@@ -26,16 +26,6 @@ CREATE TABLE Applies_For_Leave_Period(
     PRIMARY KEY(email, startDate, endDate)
 );
 
-<<<<<<< HEAD
-INSERT INTO Caretaker VALUES ('asdfasdf@gmail.com', 'asdfasdf', 'nameasdf', FALSE, 'im a weird caretaker', 98798787, 'orchard', '123123', 3);
-INSERT INTO Caretaker VALUES ('wincent@gmail.com', 'Caretaker2', 'wincent', FALSE, 'im awesome', 89712322, 'orchard', '543123', 5);
-
-INSERT INTO Full_Time_Employee VALUES ('asdfasdf@gmail.com');
-INSERT INTO Full_Time_Employee VALUES ('wincent@gmail.com');
-
-INSERT INTO applies_for_leave_period VALUES ('wincent@gmail.com', '10/10/2020', '11/10/2020', FALSE);
-INSERT INTO applies_for_leave_period VALUES ('asdfasdf@gmail.com', '10/10/2020' ,'11/10/2020', FALSE);
-=======
 CREATE TABLE AppUser
 (
   username VARCHAR PRIMARY KEY,
@@ -49,4 +39,3 @@ CREATE TABLE AppUser
   postalCode VARCHAR NOT NULL
 );
 
->>>>>>> master
