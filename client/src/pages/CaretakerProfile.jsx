1e1b2d24c8cee4b13af8a2fda95cbb35e1eb1a6a
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-07 12:27:18 +0800

    Updated UI for Caretaker profile

--- client/src/pages/CaretakerProfile.jsx
+++ client/src/pages/CaretakerProfile.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, Grid, Chip, Box, CardContent, Container, Typography } from '@material-ui/core';
 import api from '../api';
+import ReviewsSection from '../lab/ReviewsSection';
 
 /** This is a dummy component for demo purposes. The actual one will look quite different. */
 const CaretakerProfile = (props) => {
@@ -13,7 +14,9 @@ const CaretakerProfile = (props) => {
     // phonenumber: '999',
     // address: 'nus',
     // bio: ' i like carrots',
+    //
   });
+  const [services, setServices] = useState([]);
 
   makeStyles((theme) => ({
     root: {
@@ -27,16 +30,46 @@ const CaretakerProfile = (props) => {
     api.caretakers.getCaretaker(handle).then((res) => {
       setCaretaker(res);
     });
-  });
+    function getServices() {
+      return [
+        { name: 'grooming', petCategory: 'dog' },
+        { name: 'grooming', petCategory: 'cat' },
+        { name: 'walk', petCategory: 'dog' },
+      ];
+    }
+    setServices(
+      getServices().map((s) => {
+        s.message = s.name + ' for ' + s.petCategory;
+        return (
+          <Box component="span" mr={1}>
+            <Chip label={s.message} />
+          </Box>
+        );
+      }),
+    );
+  }, [handle]);
 
   return (
-    <Card>
-      <CardContent>
-        <Typography>{'Caretaker Profile'}</Typography>
-        <Typography color="primary">{`Name: ${caretaker.name}`}</Typography>
-        <Typography color="secondary">{`Email: ${caretaker.email}`}</Typography>
-      </CardContent>
-    </Card>
+    <Container>
+      <Card>
+        <CardContent>
+          <Box mb={3}>
+            <Typography style={{ color: 'red' }} variant="h4">{`${caretaker.name}`}</Typography>
+          </Box>
+          <Typography variant="h6">{'About'}</Typography>
+          <Typography paragraph>{`${caretaker.bio}`}</Typography>
+
+          <Typography variant="h6">{'Address'}</Typography>
+          <Typography paragraph>{`${caretaker.address}`}</Typography>
+
+          <Typography variant="h6">{'Email'}</Typography>
+          <Typography paragraph>{`${caretaker.email}`}</Typography>
+
+          <Typography variant="h6">{'Services I offer'}</Typography>
+          <Box mt={1}>{services}</Box>
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 
