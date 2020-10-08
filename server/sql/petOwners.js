69b749da8b57e784f5dd3247b78352102408a233
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-08 23:15:43 +0800

    Update server/sql/petOwners.js
    Co-authored-by: dorcastan <46101221+dorcastan@users.noreply.github.com>

--- server/sql/petOwners.js
+++ server/sql/petOwners.js
@@ -3,7 +3,7 @@ const petOwners = {};
 petOwners.queries = {
   index: 'SELECT * FROM pet_owners',
   view:
-    'SELECT * FROM AppUser WHERE username LIKE $1 AND $1 IN (SELECT petOwnerUsername from Caretaker)',
+    'SELECT * FROM AppUser WHERE username = $1 AND $1 IN (SELECT petOwnerUsername from Pet_Owner)',
 };
 
 module.exports = petOwners;
