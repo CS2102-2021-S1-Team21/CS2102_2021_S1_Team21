37250c2a85c21657dde71a7a4e24ac9a53d54004
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-08 23:15:34 +0800

    Update server/sql/caretakers.js
    Co-authored-by: dorcastan <46101221+dorcastan@users.noreply.github.com>

--- server/sql/caretakers.js
+++ server/sql/caretakers.js
@@ -3,7 +3,7 @@ const caretakers = {};
 caretakers.queries = {
   index: 'SELECT * FROM Caretaker',
   view:
-    'SELECT * FROM AppUser WHERE username LIKE $1 AND $1 IN (SELECT caretakerUsername from Caretaker)',
+    'SELECT * FROM AppUser WHERE username = $1 AND $1 IN (SELECT caretakerUsername from Caretaker)',
 };
 
 module.exports = caretakers;
