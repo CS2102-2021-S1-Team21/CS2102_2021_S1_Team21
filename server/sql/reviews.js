abfadd0d05b14ac6dc423539a327b316cbf00bbc
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-08 23:17:49 +0800

    Update server/sql/reviews.js
    Co-authored-by: dorcastan <46101221+dorcastan@users.noreply.github.com>

--- server/sql/reviews.js
+++ server/sql/reviews.js
@@ -3,7 +3,7 @@ const reviews = {};
 reviews.queries = {
   index: 'SELECT * FROM Tagged_To_Review',
   view:
-    "SELECT ROW_NUMBER() OVER (ORDER BY reviewDateTime) AS id, *, to_char(reviewDateTime, 'DD/MM/YYYY') AS postedOn FROM Bidded_For_Job WHERE caretakerUsername LIKE $1",
+    "SELECT ROW_NUMBER() OVER (ORDER BY reviewDateTime) AS id, *, to_char(reviewDateTime, 'DD/MM/YYYY') AS postedOn FROM Bidded_For_Job WHERE caretakerUsername = $1",
 };
 
 module.exports = reviews;
