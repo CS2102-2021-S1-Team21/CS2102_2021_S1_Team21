28263ba1d36c2ab776e504b8e4669832e204e6f9
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2020-10-09 01:48:56 +0800

    Allow admin login

--- server/auth/index.js
+++ server/auth/index.js
@@ -5,26 +5,27 @@ const LocalStrategy = require('passport-local').Strategy;
 const db = require('../db');
 
 function findUser(username, callback) {
-  db.query('SELECT * FROM AppUser WHERE username = $1', [username], (err, data) => {
-    if (err) {
-      return callback(err);
-    }
-    if (data.rows.length === 0) {
-      // TODO: logging
-      return callback(null, false);
-    }
-    if (data.rows.length !== 1) {
-      return callback(new Error('Multiple users with the same username')); // programmer's error
-    }
+  db.query(
+    `SELECT username, email, passwordDigest, name, 'user' AS type FROM AppUser WHERE username = $1
+     UNION
+     SELECT username, email, passwordDigest, name, 'admin' AS type FROM PCS_Administrator WHERE username = $1`,
+    [username],
+    (err, data) => {
+      if (err) {
+        return callback(err);
+      }
+      if (data.rows.length === 0) {
+        // TODO: logging
+        return callback(null, false);
+      }
+      if (data.rows.length !== 1) {
+        return callback(new Error('Multiple users with the same username')); // programmer's error
+      }
 
-    const user = data.rows[0];
-    return callback(null, {
-      username: user.username,
-      email: user.email,
-      passwordDigest: user.passworddigest, // case sensitive
-      name: user.name,
-    });
-  });
+      const user = data.rows[0];
+      return callback(null, user);
+    },
+  );
 }
 
 // ------------------ SESSIONS ------------------
@@ -40,6 +41,7 @@ passport.deserializeUser((username, done) => {
 });
 
 // ------------------ AUTH STRATEGY ------------------
+// Used for logging in
 
 const localStrategy = new LocalStrategy((username, password, done) => {
   findUser(username, async (error, user) => {
@@ -52,7 +54,7 @@ const localStrategy = new LocalStrategy((username, password, done) => {
     }
 
     try {
-      const passwordIsValid = await bcrypt.compare(password, user.passwordDigest);
+      const passwordIsValid = await bcrypt.compare(password, user.passworddigest);
       if (!passwordIsValid) {
         return done(null, false);
       }
@@ -63,12 +65,8 @@ const localStrategy = new LocalStrategy((username, password, done) => {
   });
 });
 
-// const authMiddleware =
-// const antiMiddleware =
-
 function init() {
   passport.use(localStrategy);
-  // passport.authMiddleware =
 }
 
 exports.init = init;
