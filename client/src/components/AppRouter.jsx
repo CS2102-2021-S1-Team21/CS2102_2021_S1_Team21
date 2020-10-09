7c5e26358ff161020531d4a07f6e6a0eef3b1942
Wincent <wincenttsjoi@gmail.com>
2020-10-09 12:32:58 +0800

    Add back formtest

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
 import AuthRouter from './AuthRouter';
+import FormTest from '../lab/FormTest';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Switch>
         {/* Non-authenticated routes */}
+        <Route exact path="/test/forms" component={FormTest} />
         <Route exact path="/login" component={Login} />
         {/* <Route exact path="/forgot-password" component={ForgotPassword} /> */}
         {/* <Route exact path="/reset-password" component={ResetPassword} /> */}
