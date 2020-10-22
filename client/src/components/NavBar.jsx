6332dbbfaf9844fb25f7d170ddfc6a8d57c706c8
dorcastan <46101221+dorcastan@users.noreply.github.com>
2020-10-22 21:18:36 +0800

    Update client/src/components/NavBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -135,7 +135,7 @@ const NavBar = ({ children }) => {
                   {'My Availability'}
                 </Button>
               )}
-              {!user.isPartTimeCaretaker && !user.isFullTimeCaretaker && !user.isPetOwner && (
+              {user.isAdmin && (
                 <Button
                   color="inherit"
                   className={classes.navItem}
