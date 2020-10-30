b533c45956d4592f1c1c4325b26c3994c028ec11
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-31 01:30:21 +0800

    Fix for codefactor

--- client/src/pages/pcsAdmin/AdminDashboard.jsx
+++ client/src/pages/pcsAdmin/AdminDashboard.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import { Container, makeStyles, Paper } from '@material-ui/core';
+import { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core';
 import api from '../../api';
 
 const useStyles = makeStyles((theme) => ({
