4441171098430da3461c62dfc4e8f555544b9e79
YangJiyu98 <yang.jiyu.jy@gmail.com>
2020-11-03 15:28:17 +0800

    Remove redundant code

--- routes/caretakers.js
+++ routes/caretakers.js
@@ -4,7 +4,6 @@ const caretakers = require('../controllers/caretakers');
 const router = express.Router();
 
 router.get('/', caretakers.index);
-router.get('/browse', caretakers.browse);
 router.get('/:username', caretakers.view);
 
 module.exports = router;
