751e6fa1dfaaad9fef14a826b63112328fa1b5c2
Dorcas Tan <46101221+dorcastan@users.noreply.github.com>
2020-11-04 22:56:00 +0800

    Add withdrawal of Pending bids (backend+frontend)

--- controllers/bids.js
+++ controllers/bids.js
@@ -30,14 +30,11 @@ exports.apply = async (req, res) => {
       ],
     );
     console.log(result.rows);
-    res.json({success: `Bid applied successfully!`, result: result.rows});
+    res.json({ success: `Bid applied successfully!`, result: result.rows });
   } catch (err) {
-     if (err) {
-
-      res
-        .status(400)
-        .json({ error: err.message });
-     }
+    if (err) {
+      res.status(400).json({ error: err.message });
+    }
   }
 };
 
@@ -118,20 +115,54 @@ exports.updateBids = async (req, res, next) => {
       ],
     );
     console.log(result.rows.transactiondatetime);
-    res.json({success: `Bid successfully updated!`, result: result.rows});
-
+    res.json({ success: 'Bid successfully updated!', result: result.rows });
   } catch (err) {
     if (err.code === errorCodes.TRANSACTION_DATE_TIME_NOT_VALID_DATETIME) {
-          console.error('ERROR: ', err.message);
+      console.error('ERROR: ', err.message);
 
-      res
-        .status(400)
-        .json({ error: messages.PAYMENT_NOT_DONE });
+      res.status(400).json({ error: messages.PAYMENT_NOT_DONE });
       return;
     }
     next(err);
-   
+
     // res.json({ error: 'An unexpected error occurred' });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const {
+      petName,
+      petOwnerUsername,
+      caretakerUsername,
+      submittedAt,
+      startDate,
+      endDate,
+    } = req.params;
+    const result = await db.query(
+      `DELETE FROM Bids
+       WHERE petname = $1 
+         AND petOwnerUsername = $2
+         AND caretakerUsername = $3
+         AND submittedAt = $4
+         AND startDate = DATE($5) 
+         AND endDate = DATE($6)
+       RETURNING *`,
+      [petName, petOwnerUsername, caretakerUsername, submittedAt, startDate, endDate],
+    );
+    if (result.rowCount == 0) {
+      res.json({
+        error: 'Unable to delete bid',
+      });
+      return;
+    }
+    res.json({
+      success: 'Bid successfully deleted!',
+    });
+  } catch (err) {
+    res.json({
+      error: 'An unexpected error occurred',
+    });
   }
 };
 
