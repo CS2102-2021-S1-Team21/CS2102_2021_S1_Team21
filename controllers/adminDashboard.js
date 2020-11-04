6c6a0026761fe7d854bf98953d32d269ccb36a57
Wincent Tjoi <46394439+Wincenttjoi@users.noreply.github.com>
2020-11-04 17:49:53 +0800

    Update controllers/adminDashboard.js
    Co-authored-by: dorcastan <46101221+dorcastan@users.noreply.github.com>

--- controllers/adminDashboard.js
+++ controllers/adminDashboard.js
@@ -27,10 +27,6 @@ exports.performance = async (req, res) => {
     const result = await db.query(
       `SELECT ROW_NUMBER() OVER () AS id, * FROM admin_summary`,
     );
-    if (result.rowCount === 0) {
-      res.json({ info: 'No caretakers exist this month' });
-      return; // TODO: next()?
-    }
     res.json(result.rows);
   } catch (err) {
     console.error('ERROR: ', err.message);
